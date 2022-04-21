5d4162d3b186d978c4826f8c156aaca0b58d43f5
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-04-22 00:39:31 +0400

    Added MyClass

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 
+
+//Классы
+
 namespace Classes
 {
-    internal class Program
+    // Создаем класс с именем MyClass.
+    // В теле класса создаем открытое поле типа string с именем field и метод с именем Method.
+
+    class MyClass
+    {
+        public string field;
+
+        public void Method()
+        {
+            Console.WriteLine(field);
+        }
+    }
+
+
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // 1. Создаем экземпляр класса MyClass (по сильной ссылке).
+            // 2. Создаем экземпляр класса MyClass с именем instance.
+            // 3. Инстанцируем класс MyClass.
+
         }
     }
 }
