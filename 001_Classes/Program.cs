bb9ebe8f703993810fe43108bc33eb59e995acd7
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-04-23 00:02:35 +0400

    Update project 001Classes and remove project Classes

--- 001_Classes/Program.cs
+++ 001_Classes/Program.cs
@@ -24,9 +24,29 @@ namespace Classes
         static void Main(string[] args)
         {
             // 1. Создаем экземпляр класса MyClass (по сильной ссылке).
+            // 1. Create an instance of class MyClass with strong reference
             // 2. Создаем экземпляр класса MyClass с именем instance.
             // 3. Инстанцируем класс MyClass.
+            // 4. Создаем переменную с именем instance, типа MyClass и присваиваем ей адрес экземпляра на куче.
+            // (instance - является ссылкой на экземпляр класса MyClass построенный на куче)
 
+            MyClass instance = new MyClass(); // это сильная ссылка //this is a strong reference
+            
+            // new MyClass().Method(); // это слабая ссылка... одноразовая  // this is a weak reference
+
+            // Полю field, экземпляра instance, присваиваем значение Hello world!
+            // To field field of a instance instance, assign value Hello world!
+            instance.field = "Hello world!";
+
+            // Выводим на экран значение поля field экземпляра instance.
+
+            Console.WriteLine(instance.field);
+
+            // Вызов метода с именем Method на экземпляре instance.
+            instance.Method();
+
+            // Delay
+            Console.ReadeLine();
         }
     }
 }
