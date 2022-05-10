4e9a34f025d8601944b501caf2688ee4b805ceaa
Hmayak Davtyan <davtyan.hmayak@gmail.com>
2022-05-10 21:43:54 +0400

    Added properties projects

---
+++ 004_Properties/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+// Классы. (Использование свойств, для доступа к закрытым полям.)
+
+namespace Classes
+{
+    class MyClass
+    {
+        private string field = null;
+
+        public string Field
+        {
+            set
+            {
+                if (value == "Goodbye")
+                    Console.WriteLine("Вы ввели недопустимое значение. Повторите попытку.");
+                else
+                    field = value;
+            }
+            get
+            {
+                if (field == null)
+                    return "В поле field отсутствуют данные.";
+                else if (field == "hello world")
+                    return field.ToUpper() + "!";
+                else
+                    return field;
+            }
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass instance = new MyClass();
+
+            instance.Field = "Goodbye";
+            Console.WriteLine(instance.Field);
+
+            Console.WriteLine(new string('-',50));
+
+            instance.Field = "hello world";
+            Console.WriteLine(instance.Field);
+
+            // Delay.
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 005_Properties/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+// Классы. Свойства с одным методом доступа.
+
+namespace Classes
+{
+    internal class Program
+    {
+        class Constants
+        {
+            private double pi = 3.14d;
+            private double e = 2.71d;
+
+            // Свойства только для записи. WriteOnly Property
+            public double PI
+            {
+                set { pi = value; }
+            }
+
+            // Свойства только для чтения. ReadOnly Property
+            public double E
+            {
+                get { return pi; }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Constants constants = new Constants();
+
+            constants.PI = 3.14159265D;
+            //Console.WriteLine(constants.PI); // Недоступно.
+
+            //constants.E = 3.71D;             // Недоступно.
+            Console.WriteLine("e = {0}", constants.E);
+
+            // Delay
+            Console.ReadKey();
+        }
+    }
+}
